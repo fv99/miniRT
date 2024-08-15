6c96e67d718a6ebd82ee38f51eef6104076dae3e
fv99 <filipvon@protonmail.com>
2024-08-15 14:46:36 +0200

    Added comments and new vector functions

--- utils_vec.c
+++ utils_vec.c
@@ -6,32 +6,39 @@
 /*   By: fvonsovs <fvonsovs@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 15:30:59 by fvonsovs          #+#    #+#             */
-/*   Updated: 2024/08/13 13:41:58 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2024/08/15 14:46:36 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+// subtracts two vectors
 t_float_3   vec_sub(t_float_3 a, t_float_3 b)
 {
     return (t_float_3){a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
+// adds two vectors
 t_float_3   vec_add(t_float_3 a, t_float_3 b)
 {
     return (t_float_3){a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
+// multiples a vector by a scalar
 t_float_3   vec_mul(t_float_3 a, float b)
 {
     return (t_float_3){a.x * b, a.y * b, a.z * b};
 }
 
+// divides a vector by a scalar
 t_float_3   vec_div(t_float_3 a, float b)
 {
     return (t_float_3){a.x / b, a.y / b, a.z / b};
 }
 
+// computes dot product of two vectors
+// dot product = two vectors return a single cscalar value
+// measures how much two vectors are alignedwith each other
 float       vec_dot(t_float_3 a, t_float_3 b)
 {
     return a.x * b.x + a.y * b.y + a.z * b.z;
--- utils_vec2.c
+++ utils_vec2.c
@@ -6,25 +6,40 @@
 /*   By: fvonsovs <fvonsovs@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 15:48:39 by fvonsovs          #+#    #+#             */
-/*   Updated: 2024/08/13 14:24:31 by fvonsovs         ###   ########.fr       */
+/*   Updated: 2024/08/15 14:46:13 by fvonsovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+// normalizes a vector
 t_float_3	vec_normalize(t_float_3 v)
 {
     float length = sqrtf(vec_dot(v, v));
     return vec_div(v, length);
 }
 
+// scales a vector by a scalar
 t_float_3   vec_scale(t_float_3 vec, float scale)
 {
     return (t_float_3){vec.x * scale, vec.y * scale, vec.z * scale};
 }
 
-float vec_length(t_float_3 vec)
+// calculates the magnitude of a vector
+float       vec_length(t_float_3 vec)
 {
     return sqrtf(vec.x * vec.x + vec.y * vec.y + vec.z * vec.z);
+}
+
+// calculates cross product
+// takes two vectors, returns a vector that is perpendicular to the first two
+t_float_3 vec_cross(t_float_3 v1, t_float_3 v2)
+{
+    t_float_3 res;
+
+    res.x = v1.y * v2.z - v1.z * v2.y;
+    res.y = v1.z * v2.x - v1.x * v2.z;
+    res.z = v1.x * v2.y - v1.y * v2.x;
+    return res;
 }
 
