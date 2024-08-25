5d4579f9ed5247698f913948176cfa248665fdc8
KristianHlavaty <ssssssomgcreeperboom@gmail.com>
2024-08-25 18:20:13 +0200

    camera, light

--- controls.c
+++ controls.c
@@ -19,10 +19,44 @@ int	handle_destroy_notify(t_win *win)
 	exit(0);
 }
 
+// handle keypress events
+// move camera in x, y, z direction arrow keys
+// move camera in z direction with w, s keys
+// same for light source
 int	handle_keypress(int keysym, t_win *win)
 {
 	if (keysym == KEY_ESC)
 		handle_destroy_notify(win);
+	else if (keysym == KEY_UP)
+	{
+		win->map->cam.vec.y -= 0.1;
+		win->map->light.pos.y -= 5.0;
+	}
+	else if (keysym == KEY_DOWN)
+	{
+		win->map->cam.vec.y += 0.1;
+		win->map->light.pos.y += 5.0;
+	}
+	else if (keysym == KEY_LEFT)
+	{
+		win->map->cam.vec.x -= 0.1;
+		win->map->light.pos.x -= 5.0;
+	}
+	else if (keysym == KEY_RIGHT)
+	{
+		win->map->cam.vec.x += 0.1;
+		win->map->light.pos.x += 5.0;
+	}
+	else if (keysym == KEY_W)
+	{
+		win->map->cam.vec.z -= 0.1;
+		win->map->light.pos.z -= 5.0;
+	}
+	else if (keysym == KEY_S)
+	{
+		win->map->cam.vec.z += 0.1;
+		win->map->light.pos.z += 5.0;
+	}
 	return (0);
 }
 
